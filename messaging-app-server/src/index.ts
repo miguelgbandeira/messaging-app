9f837b0154ba99784643d77af676eeee67c1fc72
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-06-11 18:21:18 +0100

    Add cors and .env

--- messaging-app-server/src/index.ts
+++ messaging-app-server/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 4000;
+
+app.use(cors());
 
 app.get("/", (req, res, next) => {
   res.send("Hello Word!");
