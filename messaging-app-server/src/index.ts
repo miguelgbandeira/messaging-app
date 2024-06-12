f95201b9284b8e5b661e6f62c951738f125e1304
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-06-12 09:54:16 +0100

    Fix typescript issues

--- messaging-app-server/src/index.ts
+++ messaging-app-server/src/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import * as dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 
+dotenv.config();
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+const mongoDb = process.env.MONGO_CONNECTION;
+mongoose.connect(process.env.MONGO_CONNECTION!);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
 
 app.get("/", (req, res, next) => {
   res.send("Hello Word!");
