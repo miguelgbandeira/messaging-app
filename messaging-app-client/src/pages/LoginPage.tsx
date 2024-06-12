44fe352a60a0840c2beba1be437ff9393362fa9b
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-06-12 15:34:58 +0100

    Add login form

--- messaging-app-client/src/pages/LoginPage.tsx
+++ messaging-app-client/src/pages/LoginPage.tsx
@@ -2,9 +2,33 @@ import Button from "../components/Button";
 
 function LoginPage() {
   return (
-    <div style={{ border: "1px solid red" }}>
-      <h1>LoginPage</h1>
-      <Button label="Login" onClick={() => null}></Button>
+    <div className="flex items-center justify-center h-screen border border-red-500">
+      <div className="flex flex-col items-center">
+        <h1 className="mb-4">Login</h1>
+        <form className="flex flex-col items-start">
+          <div className="flex flex-col mb-4">
+            <label htmlFor="username" className="mb-1">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="border border-indigo-500 rounded-md px-2 py-1 focus:border-2"
+            />
+          </div>
+          <div className="flex flex-col mb-4">
+            <label htmlFor="password" className="mb-1">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="border border-indigo-500 rounded-md px-2 py-1 focus:border-2"
+            />
+          </div>
+        </form>
+        <Button label="Login" onClick={() => null}></Button>
+      </div>
     </div>
   );
 }
