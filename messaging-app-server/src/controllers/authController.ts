40f069c0401725daab4c860a215468f9913876d2
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-06-15 13:52:29 +0100

    Add bcrypt

--- messaging-app-server/src/controllers/authController.ts
+++ messaging-app-server/src/controllers/authController.ts
@@ -1,19 +1,32 @@
 import { RequestHandler } from "express";
+import generateToken from "../utils/generateToken";
 import UserModel from "../models/user";
 import createHttpError from "http-errors";
+import bcrypt from "bcrypt";
 
 export const createUser: RequestHandler = async (req, res, next) => {
   const { username, password } = req.body;
   try {
     const existingUser = await UserModel.findOne({ username: username });
     if (existingUser) {
-      throw createHttpError(409, "User with that username already exists");
+      throw createHttpError(
+        409,
+        "Username already taken. Please choose a different one or log in instead"
+      );
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const newUser = await UserModel.create({
       username: username,
-      password: password,
+      password: hashedPassword,
     });
-    return res.status(201).json(newUser._id);
+
+    if (newUser) {
+      generateToken(res, newUser._id);
+      return res.status(201).json(newUser._id);
+    }
   } catch (error) {
     next(error);
   }
