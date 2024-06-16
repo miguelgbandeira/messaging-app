575920e0fcb9ffe591650bb4de2a50eec64ac625
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-06-16 17:33:41 +0100

    Add fetchLoggedInUser

--- messaging-app-client/src/App.tsx
+++ messaging-app-client/src/App.tsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { User } from "./models/user";
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    async function fetchLoggedInUser() {
+      try {
+        const userRes = await fetch("http://localhost:4000/auth/sign-up");
+        const user: User = await userRes.json();
+        setUser(user);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchLoggedInUser();
+  }, []);
+
   return (
     <>
       <ToastContainer autoClose={3000} />
