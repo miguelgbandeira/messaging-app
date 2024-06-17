2b2e1c64a5af9d10d6e1dd71d45cebfbdde41893
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-06-17 10:39:50 +0100

    Add verify-token endpoint and change protect logic

--- messaging-app-server/src/middleware/authMiddleware.ts
+++ messaging-app-server/src/middleware/authMiddleware.ts
@@ -7,26 +7,18 @@ const protect: RequestHandler = async (req, res, next) => {
   if (!process.env.JWT_SECRET) {
     throw new Error("JWT_SECRET is not defined");
   }
-  const bearerHeader = req.headers["authorization"];
+  const token = req.cookies.jwt;
 
-  // Check if bearer is undefined
-  if (typeof bearerHeader !== "undefined") {
-    // Split at the space
-    const bearer = bearerHeader.split(" ");
-    // Get token from array
-    const bearerToken = bearer[1];
-
-    if (bearerToken) {
-      try {
-        const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded.userId);
-        next();
-      } catch (error) {
-        throw createHttpError(403, "Not authorized, invalid token");
-      }
-    } else {
-      throw createHttpError(403, "Not authorized, no token");
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.userId);
+      next();
+    } catch (error) {
+      throw createHttpError(403, "Not authorized, invalid token");
     }
+  } else {
+    throw createHttpError(403, "Not authorized, no token");
   }
 };
 
--- messaging-app-server/src/routes/auth.ts
+++ messaging-app-server/src/routes/auth.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { createUser, login, logout } from "../controllers/authController";
+import { protect } from "../middleware/authMiddleware";
 const router = express.Router();
 
 router.post("/sign-up", createUser);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/verify-token", protect, (req, res) => {
+  res.status(200).json({ user: req.user });
+});
 
 export default router;
