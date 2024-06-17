d1ffdb6a1af0aa8f1bb5e18ddb58403b9d9804d0
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-06-17 10:07:02 +0100

    change jwt verification

--- messaging-app-server/src/middleware/authMiddleware.ts
+++ messaging-app-server/src/middleware/authMiddleware.ts
@@ -4,24 +4,29 @@ import { RequestHandler } from "express";
 import createHttpError from "http-errors";
 
 const protect: RequestHandler = async (req, res, next) => {
-  let token;
-
-  token = req.cookies?.jwt;
-
   if (!process.env.JWT_SECRET) {
     throw new Error("JWT_SECRET is not defined");
   }
+  const bearerHeader = req.headers["authorization"];
 
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.userId);
-      next();
-    } catch (error) {
-      throw createHttpError(403, "Not authorized, invalid token");
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== "undefined") {
+    // Split at the space
+    const bearer = bearerHeader.split(" ");
+    // Get token from array
+    const bearerToken = bearer[1];
+
+    if (bearerToken) {
+      try {
+        const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.userId);
+        next();
+      } catch (error) {
+        throw createHttpError(403, "Not authorized, invalid token");
+      }
+    } else {
+      throw createHttpError(403, "Not authorized, no token");
     }
-  } else {
-    throw createHttpError(403, "Not authorized, no token");
   }
 };
 
