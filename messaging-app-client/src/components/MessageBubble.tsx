c0fb23fc7d745587dbd04e7bf83acbdc2fa8ad28
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-06-20 11:07:04 +0100

    Refactor do MessaBubble

---
+++ messaging-app-client/src/components/MessageBubble.tsx
@@ -0,0 +1,42 @@
+import { Message } from "../models/message";
+
+interface MessagesBubbleProps {
+  message: Message;
+  user: string;
+}
+
+function MessageBubble({ message, user }: MessagesBubbleProps) {
+  function formatDay(timestampString: string) {
+    const timestamp = new Date(timestampString);
+
+    const day = timestamp.getUTCDate().toString().padStart(2, "0");
+    const month = (timestamp.getUTCMonth() + 1).toString().padStart(2, "0");
+    const year = timestamp.getUTCFullYear().toString().slice(-2);
+
+    return `${day}/${month}/${year}`;
+  }
+
+  function formatHour(timestampString: string) {
+    const timestamp = new Date(timestampString);
+
+    const hours = timestamp.getUTCHours().toString().padStart(2, "0");
+    const minutes = timestamp.getUTCMinutes().toString().padStart(2, "0");
+
+    return `${hours}:${minutes}`;
+  }
+
+  return (
+    <div
+      className={`rounded-full mb-5 p-3 ${message.sentFrom === user ? "bg-green-200" : "bg-gray-200"}`}
+      key={message._id}
+    >
+      <span>{message.message}</span>
+      <div className="flex gap-10 font-extralight text-xs">
+        <span>{formatDay(message.timestamp)}</span>
+        <span>{formatHour(message.timestamp)}</span>
+      </div>
+    </div>
+  );
+}
+
+export default MessageBubble;
--- messaging-app-client/src/components/MessagesContainer.tsx
+++ messaging-app-client/src/components/MessagesContainer.tsx
@@ -1,5 +1,6 @@
 import useData from "../hooks/useData";
 import { Message } from "../models/message";
+import MessageBubble from "./MessageBubble";
 
 interface MessagesContainerProps {
   chatId: string | null;
@@ -11,18 +12,6 @@ function MessagesContainer({ chatId, user }: MessagesContainerProps) {
     chatId ? `/messages/${chatId}` : null
   );
 
-  function formatDate(timestampString: string) {
-    const timestamp = new Date(timestampString);
-
-    const day = timestamp.getUTCDate().toString().padStart(2, "0");
-    const month = (timestamp.getUTCMonth() + 1).toString().padStart(2, "0");
-    const year = timestamp.getUTCFullYear().toString().slice(-2);
-    const hours = timestamp.getUTCHours().toString().padStart(2, "0");
-    const minutes = timestamp.getUTCMinutes().toString().padStart(2, "0");
-
-    return `${day}/${month}/${year} ${hours}:${minutes}`;
-  }
-
   if (error) return <div>No messages yet.</div>;
   if (loading) return <p>Loading...</p>;
 
@@ -30,15 +19,7 @@ function MessagesContainer({ chatId, user }: MessagesContainerProps) {
     <>
       {!data && <p>Send a message to start a conversation!</p>}
       {data &&
-        data.map((message) => (
-          <div
-            className={`flex gap-2 rounded-full mb-5 ${message.sentFrom === user ? "bg-green-200" : "bg-gray-200"}`}
-            key={message._id}
-          >
-            <span>{message.message}</span>
-            <span>{formatDate(message.timestamp)}</span>
-          </div>
-        ))}
+        data.map((message) => <MessageBubble message={message} user={user} />)}
     </>
   );
 }
